d2e36cc3a13d59c8cb665e71626f03d5912354de
Mysoft <mysoft@Users-Mac-mini.local>
2022-03-29 15:19:53 +0600

    cdap auth

---
+++ src/CdapAuth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mdsami\CdapAuth ;
+
+use Illuminate\Support\Manager;
+use Illuminate\Http\Request;
+
+class CdapAuth extends Manager implements Contracts\Factory{
+
+	private $config;
+
+
+
+}
---
+++ src/CdapAuthServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mdsami\CdapAuth ;
+use Illuminate\Support\ServiceProvider;
+
+class CdapAuthServiceProvider extends ServiceProvider {
+
+	
+
+
+
+}
